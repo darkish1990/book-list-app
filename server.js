e71d390611322214aec1830d3071ae2ee85c6366
Roey liba <rliba1990@gmail.com>
2022-05-22 11:32:10 +0300

    update server

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use("/api", indexRouter);
-app.use(express.static("public"));
+app.use(express.static("client/build"));
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+);
 const dbUrl = process.env.MONGO_URI || "mongodb://localhost:27017/BookDB";
 mongoose.connect(dbUrl, { useNewUrlParser: true });
 const db = mongoose.connection;
